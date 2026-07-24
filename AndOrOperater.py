a378af67ac7552e16c67c32e6f51030808eb6b01
advikasrivastava27-gif <advikasrivastava27@gmail.com>
2026-07-23 18:42:59 -0700

    Add files via upload

---
+++ AndOrOperater.py
@@ -0,0 +1,23 @@
+a=63
+b=0
+c=19
+
+if a and b and c:
+    print("All numbers have a boolean of True")
+else:
+    print("Atleast on number has a boolean of False")
+
+
+a=5
+b=-66
+c=0
+
+if a>0 or b>0:
+    print("Either of the number is greater than 0")
+else:
+    print("No number is greater than 0")
+
+if b>0 or c>0:
+    print("either of the number is greater than 0")
+else:
+    print("no number is greater than 0")
